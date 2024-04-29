ad720a36e26b542baa3e86cc5c883b5e64afc298
nikita <nikitos7626@mail.ru>
2024-04-29 14:40:19 +0300

    Добавил crud операцию для билетов

--- park-server/controller/ticketController.js
+++ park-server/controller/ticketController.js
@@ -1,15 +1,40 @@
 const ApiError = require('../error/Apierror')
+const {Ticket} = require('../models/models')
+
+
 class ticketController{
-    async create(req,res){
 
+    async create(req,res,next){
+        try{
+            const {code,price} = req.body
+            const ticket = await Ticket.create({code,price})
+            return res.json(ticket)
+        }
+        catch(e){
+        next(ApiError.badRequest(e.message));
+        }
+}
+
+    async getAll (req,res,next){
+        try{
+            const tickets = await Ticket.findAll();
+            return res.json(tickets);
+            
+        }catch(e){
+            next(ApiError.badRequest(e.message));
+        }
     }
 
-    async getAll (req,res){
-    }
-
-    async delete(req,res){
-
-    }
+    async delete(req,res,next){
+        try{
+        const {id} = req.params;
+        await Ticket.destroy({where:{ticket_id:id}});
+        return res.json({message:"Ticket deleted"});
+        }
+        catch(e){
+            next(ApiError.badRequest(e.message));
+        }
+    } 
 }
 
 module.exports = new ticketController()
