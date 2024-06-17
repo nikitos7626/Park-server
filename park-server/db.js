9ddb8c99349f0a13328031e03dd1a9248e540be6
nikita <nikitos7626@mail.ru>
2024-06-17 22:31:05 +0300

    изменён логин

--- park-server/db.js
+++ park-server/db.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
     "online_park",
-    "root",
-    "root",
+    "nikitos",
+    "123",
     {
         dialect: "mysql",
         host: "158.160.134.113",
